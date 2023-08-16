b16e42b5071b4efdf6321968cdd3cc567af38e5b
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-08-16 15:05:07 +0530

    TEST

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import store from "./Store/Store/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>
